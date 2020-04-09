0881d1d6457275556d6819b8f72cff1a5ab9a5e8
Blaqnificent <bayoduduyemi@gmail.com>
2020-04-10 00:08:44 +0100

    Final

--- Python_task_2.py
+++ Python_task_2.py
@@ -31,34 +31,36 @@ while program_status:
     user_deets_entry = user_deets()
     rand_pass = random_password_gen(user_deets_entry)
     print()
-    print("Your randomly genearted password is " + rand_pass + ", would you like to keep it?")
+    print("Your randomly genearted password is " + rand_pass + ", would you like to keep it?") #Generates a random password from the user's first and last name
     usr_choice = input ("Enter either 'yes' to keep it,  or 'no' to choose your own: ")
-    usr_choice = usr_choice.lower()
+    usr_choice = usr_choice.lower() #Ensures uniformity of user's input choice. Always lower case
     
     password_loop = True
     while password_loop:
         if usr_choice == 'yes':
-            user_deets_entry.append(rand_pass)
-            container.append(user_deets_entry)
+            user_deets_entry.append(rand_pass) #Adds the generated password to the other user's details 
+            container.append(user_deets_entry) #Adds the user's details to a container
             password_loop = False
         
         else:
             print ("Enter a password with at least 7 characters below: ")
             usr_choice = input()
-            while len(usr_choice) < 7:
+            while len(usr_choice) < 7: #Keeps asking the user to enter a password till the length is 7 characters or longer
                 print ("Your password is less than 7 characters. Choose a longer password: ")
                 usr_choice =input()
-            if len(usr_choice) >= 7:
+            if len(usr_choice) >= 7: #If paasword is longer than 7, add to the other user's details
                 user_deets_entry.append(usr_choice)
                 container.append(user_deets_entry)
-                break 
-        new_usr_choice = input ("Would you like to enter a new user? Enter 'yes' or 'no': ")
-        new_usr_choice = new_usr_choice.lower()
+                password_loop = False #Breaks out of the password check loop
+    new_usr_choice = input ("Would you like to enter a new user? Enter 'yes' or 'no': ")
+    new_usr_choice = new_usr_choice.lower() #Ensures's user's choice is always lowercase. 
 
     if new_usr_choice == 'no':
         program_status = False
-        for item in container:
+        for item in container: #Prints out all items in container, as long as we no longer collect any more details. 
             print (item)
 
     else:
-        program_status = False
+        program_status = True
+
+##END
